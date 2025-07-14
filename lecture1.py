271963d979c306708c700b82633f9570e4901d88
Kushagra Jaiswal <kjjaiswal2003@gmail.com>
2025-07-14 14:04:09 +0530

    Python Introduction and Variable and Data Types

---
+++ lecture1.py
@@ -0,0 +1,85 @@
+# first program
+print("Hello World")
+
+# Variables
+
+name = "kushagra"
+age = 20
+print("my name is : " , name)
+print("my age is : " , age)
+
+print(type(name))
+print(type(age))
+
+
+age = 23
+old = False
+a = None
+print(type(age))
+print(type(old))
+print(type(a))
+
+# sum of two number
+a= 500
+b= 1000
+sum = a+b
+print(sum)
+ 
+# Arithmetic Operators 
+a= 5
+b= 2
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b) 
+
+# Relational Operator
+
+a= 50
+b= 30
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+
+print(a>=b)
+
+# type conversion
+a= 2
+b= 4.25
+sum = a+b
+print(sum)
+# type casting
+a= 34
+b= int("33")
+print(a+b)
+
+# # input in python
+# a= int(input("enter ur age : "))
+
+#Practice Problem
+
+a= int(input("Enter 1st number : "))
+
+
+b= int(input("Enter 2st number : "))
+sum = a+b
+print("The sum of two number is:",sum)
+ 
+# Problem 2 
+a= int(input("Side of the square is :"))
+area = a*a
+print("The area of the square is : ",area)
+
+# Problem 3
+a = float(input("first number is : "))
+b = float(input("second number is : "))
+print("Avg=" , (a+b)/2)
+
+# problem 4
+a= int(input("first number :"))
+b= int(input("second number :"))
+print(a>=b)
